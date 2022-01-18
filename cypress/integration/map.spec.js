cdfbdeeb423cd82b5d3b6abfc4503dcd0db03a18
ksenkso <yazunya@gmail.com>
2022-01-19 02:33:35 +0300

    refactor(test): extract common code

--- cypress/integration/map.spec.js
+++ cypress/integration/map.spec.js
@@ -3,12 +3,17 @@ context('Map interactions', () => {
     cy.visit('http://localhost:3000');
   });
 
+  const createItem = (name) => {
+    cy.findByRole('textbox').type(name + '{Enter}');
+  };
+
+  const PLACEMARK_ICON_SELECTOR = '[class*=placemark-overlay] [class*=svg-icon]';
+  const PLACEMARKS_SELECTOR = '[class*=placemark-overlay] > ymaps';
+
   it('should allow to reorder placemarks by dragging them', () => {
     const items = Array(3).fill(null).map((_, index) => `item ${index}`);
 
-    items.forEach(placemarkName => {
-      cy.findByRole('textbox').type(placemarkName + '{Enter}');
-    });
+    items.forEach(createItem);
 
     cy.findAllByText(/item/).should('have.length', 3);
 
@@ -26,16 +31,14 @@ context('Map interactions', () => {
   it('should create placemarks on the map', () => {
     const items = Array(3).fill(null).map((_, index) => `item ${index}`);
 
-    items.forEach(placemarkName => {
-      cy.findByRole('textbox').type(placemarkName + '{Enter}');
-    });
+    items.forEach(createItem);
 
-    cy.get('[class*=placemark-overlay] > ymaps').should('have.length', 3);
+    cy.get(PLACEMARKS_SELECTOR).should('have.length', 3);
   });
 
   it('should show a baloon with placemark text when placemark is clicked', () => {
-    cy.findByRole('textbox').type('item name{Enter}');
-    cy.get('[class*=placemark-overlay] [class*=svg-icon]').first().then(icon => {
+    createItem('item name');
+    cy.get(PLACEMARK_ICON_SELECTOR).first().then(icon => {
       const position = icon.offset();
       cy.get('#map').click(position.left + 10, position.top + 10);
       cy.get('#map').findByText('item name').should('be.visible');
